5cf5c7f9b1cc3ca5873e3b49d75ef5b2eebbccf8
NigelMarshal <nigelmarshal@gmail.com>
2021-11-04 11:30:06 +0400

    feat(meetups): Add meetup components and render with dummy data

--- src/Components/Layout/MainNavigation.js
+++ src/Components/Layout/MainNavigation.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 
+import classes from './MainNavigation.module.css';
 const MainNavigation = () => {
   return (
-    <header>
-      <div>Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Meetups</div>
       <nav>
         <ul>
           <li>
--- src/Pages/AllMeetups.js
+++ src/Pages/AllMeetups.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import MeetupList from '../Components/Layout/Meetups/MeetupList';
+const TEST_DATA = [
+    {
+        id: 'm1',
+        title: 'Meetup 1',
+        image:
+          'https://via.placeholder.com/350x150',
+        address: '245 Street',
+        description:
+          'Pub?!',
+      },
+      {
+        id: 'm2',
+        title: 'Meetup2',
+        image:
+          'https://via.placeholder.com/350x150',
+        address: '123 Street',
+        description:
+          'Meet me at the phoooone',
+      },
+];
 
 const AllMeetupsPage = () => {
     return (
-        <div>
-            All Meetups Page
-        </div>
+     <section>
+         <h1>All Meetups</h1>
+          <MeetupList meetups={TEST_DATA}/>
+     </section>
     )
 }
 
